c04b0fe0e7fc8e91d5e64d52b848c10a1feee032
z.asim <72907332+hahazz4@users.noreply.github.com>
2023-01-23 16:03:22 -0500

    first commit

---
+++ register.v
@@ -0,0 +1,13 @@
+module Register(clk, clr, enable, D, Q);
+    input [31:0] D;
+    input clk, clr, enable;
+    output reg [31:0] Q;
+
+    always @(posedge clk) //posedge -> positive edge trigger, negedge -> negative edge trigger
+        if(clr)
+            Q <= 0;
+
+        else if(enable)
+            Q = D; //Data stored in Q, if the enable is 1.
+            
+endmodule //Register end.
