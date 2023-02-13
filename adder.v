9f2faf6b446a3ce7de1f295f2a4ed73fb3490a96
z.asim <Z_zee09@hotmail.com>
2023-02-13 02:13:49 -0500

    starting to implement adder

---
+++ adder.v
@@ -0,0 +1,13 @@
+//Start of code
+
+//Carry Look Ahead Adder
+module carry_out(input reg [15:0] 
+                  output [31:0] );
+    
+    always @(negedge clk){
+        begin
+
+        end
+    }    
+endmodule
+//End of code
