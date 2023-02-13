6f6542620b3b5cedd545f2f12abd31769d99b760
z.asim <72907332+hahazz4@users.noreply.github.com>
2023-02-13 17:13:20 -0500

    fixing the non restoring division algorithm

--- divider.v
+++ divider.v
@@ -1,41 +1,60 @@
 //Start of code
-module multiplier(input reg [15:0] Q, B, A, n, Q0, slres,            //Q is dividend, B is divisor, n is the number of bits for B, and rem_result is the remainder, div_result is final dividend.
-                  output [31:0] div_result, rem_result);
+//Non-Restoring Division Algorithm
+module divider(input reg [31:0] Q, B, A, Q0, slres, tc,              //Q is dividend, B is divisor, n is the number of bits for B, and rem_result is the remainder, div_result is final dividend.
+               integer n,
+               output [31:0] div_result, rem_result);
     
     always @(negedge clk){
         begin
-            n = $size(Q)
-            A = (n+1)'b0;                                            //Initializing A as 0000.
+            n = 31;
+            if(Q < 0)
+            begin
+                tc = ~Q + 'b1;
+                Q = tc;
+            end
+
+            else if(B < 0)
+            begin
+                tc = ~B + 'b1;
+                B = tc;
+            end
+
+            else if(Q < 0 && B < 0)
+            begin
+                tc = ~Q + 'b1;
+                Q = tc;
+                tc = ~B + 'b1;
+                B = tc;
+            end
+
+            A = (32)'b0;                                            //Initializing A as 0000.
 
             for (i = 0; i < n; i++)                                  //Iterating through n number of loops
             begin
                 slres = A << 1;
-                if(i < n)                                            //when i is less than the number of bits, n.
-                begin
-                    if(A == (1)'b0)                                  //If the sign bit of A is 0
-                    begin                      
-                        A = A - B;
-                        Q0 = 'b1;
-                        Q = (1)Q0;
-                    end
-                    
-                    else if(A == (1)'b1)                             //If the sign bit of A is 1
-                    begin
-                        A = A + B;
-                        Q0 = 'b0;
-                        Q = (1)Q0;
-                    end
+                if(A[31] == 'b0)                                 //If the less significant bit of A is 0
+                begin                      
+                    A = A - B;
+                    Q[31] = 'b1;                                  //Q0 = 1
+                    // Q = (1)Q0;
                 end
                 
-                else                                                 //when i is equal to the number of bits, n.
+                else if(A[31] == 'b1)                             //If the less significant bit of A is 1
                 begin
-                    if(A == (1)'b0)                                  //If the sign bit of A is 0
+                    A = A + B;
+                    Q[31] = 'b0;                                  //Q0 = 0
+                    // Q = (1)Q0;
+                end
+            
+                if(i == n)                                        //when i is equal to the number of bits, n.
+                begin
+                    if(A[31] == 'b0)                                  //If the less significant bit of A is 0
                     begin                      
                         div_result = Q;
                         rem_result = A;
                     end
                     
-                    else if(A == (1)'b1)                             //If the sign bit of A is 1
+                    else if(A[31] == 'b1)                             //If the less significant bit of A is 1
                     begin
                         A = A + B;
                         div_result = Q;
