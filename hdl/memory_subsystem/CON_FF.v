915a0c19acf123d931e410faae07ee41e40e2f31
z.asim <Z_zee09@hotmail.com>
2023-03-20 02:41:36 -0400

    me and uncle gpt fixed con_ff.v, select_encode_logic.v, added two other modules ;)

--- hdl/memory_subsystem/CON_FF.v
+++ hdl/memory_subsystem/CON_FF.v
@@ -1,15 +1,16 @@
-/* This module determines whether the correct condtion has been met to cause branching to take place in a conditional branch instruction. */
+/* This module determines whether the correct condition has been met to cause branching to take place in a conditional branch instruction. */
 module CON_FF(
     input [31:0] bus_in, ir, 
     input con_in_signal,
-    output con_out);                           //c2 is located at bits 22:19 of the ir register
-    
-    reg [3:0] decoder_out;
-    reg con_D, reg_1, reg_2, reg_3, reg_4, reg_5; 
+    output con_out
+);                                                
+
+    wire [3:0] decoder_out;
+    wire eq, not_eq, pos, neg, branch_flag;
     
     always @(*)
     begin
-        case(ir[1:0])
+        case((ir[1:0]))                                                 //2 to 4 decoder
             2'b00 : begin
                 decoder_out = 4'b0001;
             end
@@ -28,36 +29,11 @@ module CON_FF(
         endcase
     end
     
-    always @(con_in_signal && bus_in)
-    begin
-        if((decoder_out[0] and bus_in) == 0)        //If the decoder_out[0] AND bus_in is equal to 0, then D = 1;
-        begin
-            res_1 = 1;  
-        end
-
-        else if((decoder_out[1] and bus_in) != 0)   //If the decoder_out[1] AND bus_in is not equal to 0, then D = 1;
-        begin
-            res_2 = 1;  
-        end
-
-        else if((decoder_out[2] and bus_in) >= 0)   //If the decoder_out[2] AND bus_in greater than equal to 0, then D = 1;
-        begin
-            res_3 = 1;  
-        end
-
-        else if((decoder_out[3] and bus_in[31]))    //If the decoder_out[3] AND bus_in is equal to 1, then D = 1;
-        begin
-            res_4 = 1;  
-        end
-
-        else                                        //If the decoder_out AND bus_in is less than equal to 0, then D = 0;
-        begin
-            res_5 = 0;  
-        end
-    end
+    assign eq = (bus_in == 32'd0) ? 1'b1 : 1'b0;
+    assign not_eq = (bus_in != 32'd0) ? 1'b1 : 1'b0;
+    assign pos = (bus_in[31] == 0) ? 1'b1 : 1'b0;
+    assign neg = (bus_in[31] == 1) ? 1'b1 : 1'b0;
     
-    con_D = (res_1 or res_2 or res_3 or res_4);     //Or all the and results obtained.
-
-    assign con_out = con_D;                         //Assigning the value of con_D to the output wire con_out.
-
+    assign branch_flag = (decoder_out[0] and eq or decoder_out[1] and not_eq or decoder_out[2] and pos or decoder_out[3] and neg);                                             //Assigning the value of con_D to the output wire con_out.
+    flip_flop_logic ffl(.clk(con_in_signal), .d(branch_flag), .q(con_out));
 endmodule
--- hdl/memory_subsystem/select_encode.v
+++ hdl/memory_subsystem/select_encode.v
@@ -2,123 +2,36 @@ module select_encode(
     input [31:0] ir,
     input Gra, Grb, Grc, r_enable, r_out, ba_out,   //External inputs
     output [31:0] c_sign);
-    // output r0_select, r1_select, r2_select, r3_select, r4_select, r5_select,         //gotta check in with Nick about this...
-    // r6_select, r7_select, r8_select, r9_select, r10_select, r11_select, 
-    // r12_select, r13_select, r14_select, r15_select, r0_out, r1_out, r2_out, 
-    // r3_out, r4_out, r5_out, r6_out, r7_out, r8_out, r9_out, r10_out, r11_out, 
-    // r12_out, r13_out, r14_out, r15_out);
 
     reg [3:0] ra, rb, rc, opcode;
-    reg [15:0] decoder_in, decoder_out, in, out;
+    reg [15:0] decoder_in_a, decoder_in_b, decoder_in_c, decoder_in, decoder_out;
     reg [31:0] c_sign_ext;
 
-    always @(*)
+    assign opcode = ir[31:27];     //bit 31 -> 27 of the instruction register is the opcode.
+    assign ra = ir[26:23];         //bit 26 -> 23 of the instruction register is register a.
+    assign rb = ir[22:19];         //bit 22 -> 19 of the instruction register is register b.
+    assign rc = ir[18:15];         //bit 18 -> 15 of the instruction register is register c.
+
+    if(Gra)                 //if the General register a control signal is 1, then decoder input would be register a AND Gra.
     begin
-        opcode = ir[31:27];     //bit 31 -> 27 of the instruction register is the opcode.
-        ra = ir[26:23];         //bit 26 -> 23 of the instruction register is register a.
-        rb = ir[22:19];         //bit 22 -> 19 of the instruction register is register b.
-        rc = ir[18:15];         //bit 18 -> 15 of the instruction register is register c.
-
-        if(Gra)         //if the Ga control signal is 1, then decoder input would be register a.
-        begin
-            decoder_in = ra;
-        end
-
-        else if(Grb)    //if the Ga control signal is 1, then decoder input would be register b.
-        begin
-            decoder_in = rb;
-        end
-
-        else if(Grc)    //if the Ga control signal is 1, then decoder input would be register c.
-        begin
-            decoder_in = rc;
-        end
-
-        else            //Otherwise, let input, output = 0.
-        begin
-            in, out = 0;
-        end
-
-        case(decoder_in)
-            4'b0000 : begin
-                decoder_out = 16'b0000000000000001;
-            end
-            
-            4'b0001 : begin
-                decoder_out = 16'b0000000000000010;
-            end
-
-            4'b0010 : begin
-                decoder_out = 16'b0000000000000100;
-            end
-
-            4'b0011 : begin
-                decoder_out = 16'b0000000000001000;
-            end
-
-            4'b0100 : begin
-                decoder_out = 16'b0000000000010000;
-            end
-
-            4'b0101 : begin
-                decoder_out = 16'b0000000000100000;
-            end
-
-            4'b0110 : begin
-                decoder_out = 16'b0000000001000000;
-            end
-
-            4'b0111 : begin
-                decoder_out = 16'b0000000010000000;
-            end
-
-            4'b1000 : begin
-                decoder_out = 16'b0000000100000000;
-            end
-
-            4'b1001 : begin
-                decoder_out = 16'b0000001001000000;
-            end
-
-            4'b1010 : begin
-                decoder_out = 16'b0000010000000000;
-            end
-
-            4'b1011 : begin
-                decoder_out = 16'b0000100000000000;
-            end
-
-            4'b1100 : begin
-                decoder_out = 16'b0001000000000000;
-            end
-
-            4'b1101 : begin
-                decoder_out = 16'b0010000000000000;
-            end
-
-            4'b1110 : begin
-                decoder_out = 16'b0100000000000000;
-            end
-
-            4'b1111 : begin
-                decoder_out = 16'b1000000000000000;
-            end
-
-            default : begin
-                decoder_out = 16'b0000000000000000;
-            end
-        endcase
-
-        if(r_enable)                //if the enable signal is 1, then input is the decoder output, and the output would be 0.
-        begin
-            in = decoder_out;
-            out = 0;
-        end
-                
-        else if(ba_out | r_out)     //if the result of the (base address output signal AND register output signal) = 1, then the output = decoder output, and the input would be 0.
-        begin
-            out = decoder_out;
-            in = 0;
-        end
+        decoder_in_a = (ra and {4{Gra}});
     end
+
+    else if(Grb)            //if the General register b control signal is 1, then decoder input would be register b AND Grb.
+    begin
+        decoder_in_b = (rb and {4{Grb}});
+    end
+
+    else if(Grc)            //if the General register c control signal is 1, then decoder input would be register c AND Grc.
+    begin
+        decoder_in_c = (rc and {4{Grc}});
+    end
+
+    assign decoder_in = (decoder_in_a or decoder_in_b or decoder_in_c);     //Or's all the decoder_inputs for register a, b, c and stores the result in decoder_in.
+
+    decode_4_to_16 decode(decoder_in, decoder_out);                         //Calling the 4-to-16 decoder and passing in the parameters, decoder_in and decoder_out.
+
+    assign r_enable = {16{r_enable}} and decoder_out;
+    assign r_out = ({16{ba_out[15:0]}} or {16{r_out}}) and decoder_out;
 endmodule
+
